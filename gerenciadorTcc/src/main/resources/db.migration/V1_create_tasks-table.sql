b345602f97dcfb15dc11003c01dd60cef908178f
M.S Santtos <maxsuelsanttos3@gmail.com>
2023-09-16 15:33:52 -0300

    UPDATE SQL

--- gerenciadorTcc/src/main/resources/db.migration/V1_create_tasks-table.sql
+++ gerenciadorTcc/src/main/resources/db.migration/V1_create_tasks-table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tarefa(
-    id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     completed_status TEXT NOT NULL
